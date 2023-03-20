e8eb8a553a47750e6b65b0a982a16a511edd1d27
Chorismate <me@chorismate.dev>
2023-03-19 20:23:52 -0700

    begin writing main()

--- main.c
+++ main.c
@@ -11,21 +11,32 @@
 #include "main.h"
 
 #include "core/tasks.h"
-#include "core/blocks.h"
+//#include "core/blocks.h"
 
 const size_t TASKBLOCKSIZE = 64;
 
 int main(void) {
-	printf(
-		"Initalizing %s v%s by %s\n"
-		"Need to allocate %zu tasks.\n",
-		NAME, VERSION, AUTHOR, TASKBLOCKSIZE
-	);
+	printf("Initalizing %s v%s by %s.\n", NAME, VERSION, AUTHOR);
+	
+	printf("Need to allocate %zu tasks.\n", TASKBLOCKSIZE);
 	size_t tasks = 0;
 	size_t taskarraysize = TASKBLOCKSIZE;
-	Task *taskarray = (Task *)malloc(TASKBLOCKSIZE * sizeof(Task));
+	Task *taskarray = (Task *) malloc(TASKBLOCKSIZE * sizeof(Task));
 	
-	while (1) {
+	while(1) {
+		for(size_t i=0; i<tasks; i++) {
+			if(i == SIZE_MAX) {
+				fprintf(stderr, "How much memory does your computer have?");
+				exit(1);
+			}
+			printf(
+				"%zu. %s (due %lld epoch time)",
+				i+1, taskarray[i].name, taskarray[i].sdue
+			);
+			// TODO use human-readable notation
+		}
+		
+		/* prompt for action */
 		/* TODO */
 	}
 }
